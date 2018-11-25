e68b90cffe3e6069f6cc3bdba8dedb4eea233d22
prayzzz <prayzzz@outlook.com>
2018-11-25 13:16:50 +0100

    - initial commit

---
+++ src/PRZ.PushCenter/Controllers/DemoController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PRZ.PushCenter.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DemoController : ControllerBase
+    {
+        
+    }
+}
