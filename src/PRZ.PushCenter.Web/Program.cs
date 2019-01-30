0b7c182155911454ff0915c59d14b6b9c2fc342d
prayzzz <prayzzz@outlook.com>
2019-01-30 22:32:22 +0100

    - read settings file passed by argument

--- src/PRZ.PushCenter.Web/Program.cs
+++ src/PRZ.PushCenter.Web/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Serilog;
 
 namespace PRZ.PushCenter.Web
@@ -14,9 +17,18 @@ namespace PRZ.PushCenter.Web
         private static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
             return WebHost.CreateDefaultBuilder(args)
+                          .ConfigureAppConfiguration((context, builder) => ConfigureAppConfiguration(args, builder))
                           .UseStartup<Startup>()
                           .UseSerilog(ConfigureLogging)
                           .SuppressStatusMessages(true);
+        }
+
+        private static void ConfigureAppConfiguration(IReadOnlyList<string> args, IConfigurationBuilder builder)
+        {
+            if (args.Any() && !string.IsNullOrEmpty(args[0]))
+            {
+                builder.AddJsonFile(args[0], true);
+            }
         }
 
         private static void ConfigureLogging(WebHostBuilderContext context, LoggerConfiguration config)
