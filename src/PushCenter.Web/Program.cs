f1fc979c97d387fe4e44eaf78c2d383184cbbccf
prayzzz <prayzzz@outlook.com>
2020-09-13 15:20:38 +0200

    update dependencies

--- src/PushCenter.Web/Program.cs
+++ src/PushCenter.Web/Program.cs
@@ -9,8 +9,6 @@ namespace PushCenter.Web
 {
     public static class Program
     {
-        public const string Name = "PushCenter";
-
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
@@ -31,10 +29,7 @@ namespace PushCenter.Web
         private static void ConfigureAppConfiguration(IReadOnlyList<string> args, IConfigurationBuilder builder)
         {
             // Add JSON File passed by arguments
-            if (args.Any() && !string.IsNullOrEmpty(args[0]))
-            {
-                builder.AddJsonFile(args[0], true);
-            }
+            if (args.Any() && !string.IsNullOrEmpty(args[0])) builder.AddJsonFile(args[0], true);
         }
 
         private static void ConfigureLogging(HostBuilderContext context, LoggerConfiguration config)
